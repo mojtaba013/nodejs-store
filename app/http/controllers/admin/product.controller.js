37fdf7633be7b7054917dd09404d5fa8b9654cdc
unknown <kahroba013@gmail.com>
2023-12-02 22:05:17 +0330

    add product

--- app/http/controllers/admin/product.controller.js
+++ app/http/controllers/admin/product.controller.js
@@ -3,10 +3,35 @@ const Controller = require("../controller");
 class ProductController extends Controller{
 async addProduct(req,res,next){
     try {
-        return res.json(req.body)
-    } catch (error) {
-        
-    }
+        const images = ListOfImagesFromRequest(req?.files || [], req.body.fileUploadPath)
+        const productBody = await createProductSchema.validateAsync(req.body);
+        const { title, text, short_text, category, tags, count, price, discount, type } = productBody;
+        const supplier = req.user._id;
+        let features = setFeatures(req.body)
+        const product = await ProductModel.create({
+          title,
+          text,
+          short_text,
+          category,
+          tags,
+          count,
+          price,
+          discount,
+          images,
+          features,
+          supplier,
+          type
+        })
+        return res.status(HttpStatus.CREATED).json({
+          statusCode: HttpStatus.CREATED,
+          data: {
+            message: "ثبت محصول با موفقیت انجام شد"
+          }
+        });
+      } catch (error) {
+        deleteFileInPublic(req.body.image)
+        next(error);
+      }
 }
 
 async editProduct(req,res,next){
--- app/router/admin/product.js
+++ app/router/admin/product.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { ProductController } = require("../../http/controllers/admin/product.controller");
+const { stringToArray } = require("../../http/middlewares/stringToArray");
 const { uploadFiles } = require("../../utils/multer");
 
-router.post('/add',uploadFiles.single('image'),ProductController.addProduct);
+router.post('/add',uploadFiles.array('image',10),stringToArray('tags'),ProductController.addProduct);
 // router.patch();
 // router.delete();
 // router.get();
